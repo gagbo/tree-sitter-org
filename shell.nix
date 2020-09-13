87f892215f8ca88960a55828f8a1823163918c7a
Gerry Agbobada <gagbobada+git@gmail.com>
2020-09-13 17:05:51 +0200

    Fix shell to use gcc10

--- shell.nix
+++ shell.nix
@@ -2,7 +2,8 @@
 # Or with direnv:
 # use_nix
 { pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
+  pkgs.gcc10Stdenv.mkDerivation {
+    name="treesitter_org_shell";
     buildInputs = [ 
       pkgs.nodejs-14_x
       pkgs.gcc10
