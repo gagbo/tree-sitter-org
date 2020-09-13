8e1f51e4830cb296809d88583c1cbd59f6184271
Gerry Agbobada <gagbobada+git@gmail.com>
2020-09-13 15:17:09 +0200

    Pass simple headline test

--- grammar.js
+++ grammar.js
@@ -2,8 +2,24 @@ module.exports = grammar({
   name: 'Org',
 
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => 'hello'
+    // TODO: add the metadata header
+    org_file: $ => repeat($._section),
+
+    _section: $ => choice(
+        $.headline
+        // TODO: add other kinds
+    ),
+    
+    headline: $ => seq(
+        $.stars,
+        $.whitespace,
+        $.title
+    ),
+
+    title: $ => $.text,
+    stars: $ => /\*+/,
+    text: $ => /[^\r\n]+/,
+    whitespace: $ => /\s+/
   }
 });
 
--- src/parser.c
+++ src/parser.c
@@ -6,30 +6,48 @@
 #endif
 
 #define LANGUAGE_VERSION 11
-#define STATE_COUNT 4
-#define LARGE_STATE_COUNT 2
-#define SYMBOL_COUNT 3
+#define STATE_COUNT 9
+#define LARGE_STATE_COUNT 4
+#define SYMBOL_COUNT 9
 #define ALIAS_COUNT 0
-#define TOKEN_COUNT 2
+#define TOKEN_COUNT 4
 #define EXTERNAL_TOKEN_COUNT 0
 #define FIELD_COUNT 0
-#define MAX_ALIAS_SEQUENCE_LENGTH 1
+#define MAX_ALIAS_SEQUENCE_LENGTH 3
 
 enum {
-  anon_sym_hello = 1,
-  sym_source_file = 2,
+  sym_stars = 1,
+  sym_text = 2,
+  sym_whitespace = 3,
+  sym_org_file = 4,
+  sym__section = 5,
+  sym_headline = 6,
+  sym_title = 7,
+  aux_sym_org_file_repeat1 = 8,
 };
 
 static const char *ts_symbol_names[] = {
   [ts_builtin_sym_end] = "end",
-  [anon_sym_hello] = "hello",
-  [sym_source_file] = "source_file",
+  [sym_stars] = "stars",
+  [sym_text] = "text",
+  [sym_whitespace] = "whitespace",
+  [sym_org_file] = "org_file",
+  [sym__section] = "_section",
+  [sym_headline] = "headline",
+  [sym_title] = "title",
+  [aux_sym_org_file_repeat1] = "org_file_repeat1",
 };
 
 static TSSymbol ts_symbol_map[] = {
   [ts_builtin_sym_end] = ts_builtin_sym_end,
-  [anon_sym_hello] = anon_sym_hello,
-  [sym_source_file] = sym_source_file,
+  [sym_stars] = sym_stars,
+  [sym_text] = sym_text,
+  [sym_whitespace] = sym_whitespace,
+  [sym_org_file] = sym_org_file,
+  [sym__section] = sym__section,
+  [sym_headline] = sym_headline,
+  [sym_title] = sym_title,
+  [aux_sym_org_file_repeat1] = aux_sym_org_file_repeat1,
 };
 
 static const TSSymbolMetadata ts_symbol_metadata[] = {
@@ -37,13 +55,37 @@ static const TSSymbolMetadata ts_symbol_metadata[] = {
     .visible = false,
     .named = true,
   },
-  [anon_sym_hello] = {
-    .visible = true,
-    .named = false,
-  },
-  [sym_source_file] = {
+  [sym_stars] = {
     .visible = true,
     .named = true,
+  },
+  [sym_text] = {
+    .visible = true,
+    .named = true,
+  },
+  [sym_whitespace] = {
+    .visible = true,
+    .named = true,
+  },
+  [sym_org_file] = {
+    .visible = true,
+    .named = true,
+  },
+  [sym__section] = {
+    .visible = false,
+    .named = true,
+  },
+  [sym_headline] = {
+    .visible = true,
+    .named = true,
+  },
+  [sym_title] = {
+    .visible = true,
+    .named = true,
+  },
+  [aux_sym_org_file_repeat1] = {
+    .visible = false,
+    .named = false,
   },
 };
 
@@ -56,30 +98,53 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   eof = lexer->eof(lexer);
   switch (state) {
     case 0:
-      if (eof) ADVANCE(5);
-      if (lookahead == 'h') ADVANCE(1);
+      if (eof) ADVANCE(3);
+      if (lookahead == '*') ADVANCE(4);
       if (lookahead == '\t' ||
           lookahead == '\n' ||
           lookahead == '\r' ||
           lookahead == ' ') SKIP(0)
       END_STATE();
     case 1:
-      if (lookahead == 'e') ADVANCE(3);
+      if (lookahead == '\t' ||
+          lookahead == ' ') ADVANCE(5);
+      if (lookahead == '\n' ||
+          lookahead == '\r') SKIP(1)
+      if (lookahead != 0) ADVANCE(6);
       END_STATE();
     case 2:
-      if (lookahead == 'l') ADVANCE(4);
+      if (lookahead == '\t' ||
+          lookahead == '\n' ||
+          lookahead == '\r' ||
+          lookahead == ' ') ADVANCE(7);
       END_STATE();
     case 3:
-      if (lookahead == 'l') ADVANCE(2);
-      END_STATE();
-    case 4:
-      if (lookahead == 'o') ADVANCE(6);
-      END_STATE();
-    case 5:
       ACCEPT_TOKEN(ts_builtin_sym_end);
       END_STATE();
+    case 4:
+      ACCEPT_TOKEN(sym_stars);
+      if (lookahead == '*') ADVANCE(4);
+      END_STATE();
+    case 5:
+      ACCEPT_TOKEN(sym_text);
+      if (lookahead == '\t' ||
+          lookahead == ' ') ADVANCE(5);
+      if (lookahead != 0 &&
+          lookahead != '\n' &&
+          lookahead != '\r') ADVANCE(6);
+      END_STATE();
     case 6:
-      ACCEPT_TOKEN(anon_sym_hello);
+      ACCEPT_TOKEN(sym_text);
+      if (lookahead != 0 &&
+          lookahead != '\n' &&
+          lookahead != '\r') ADVANCE(6);
+      END_STATE();
+    case 7:
+      ACCEPT_TOKEN(sym_whitespace);
+      if (lookahead == '\t' ||
+          lookahead == '\n' ||
+          lookahead == '\r' ||
+          lookahead == ' ') ADVANCE(7);
       END_STATE();
     default:
       return false;
@@ -91,39 +156,85 @@ static TSLexMode ts_lex_modes[STATE_COUNT] = {
   [1] = {.lex_state = 0},
   [2] = {.lex_state = 0},
   [3] = {.lex_state = 0},
+  [4] = {.lex_state = 1},
+  [5] = {.lex_state = 0},
+  [6] = {.lex_state = 0},
+  [7] = {.lex_state = 2},
+  [8] = {.lex_state = 0},
 };
 
 static uint16_t ts_parse_table[LARGE_STATE_COUNT][SYMBOL_COUNT] = {
   [0] = {
     [ts_builtin_sym_end] = ACTIONS(1),
-    [anon_sym_hello] = ACTIONS(1),
+    [sym_stars] = ACTIONS(1),
   },
   [1] = {
-    [sym_source_file] = STATE(3),
-    [anon_sym_hello] = ACTIONS(3),
+    [sym_org_file] = STATE(8),
+    [sym__section] = STATE(2),
+    [sym_headline] = STATE(2),
+    [aux_sym_org_file_repeat1] = STATE(2),
+    [ts_builtin_sym_end] = ACTIONS(3),
+    [sym_stars] = ACTIONS(5),
+  },
+  [2] = {
+    [sym__section] = STATE(3),
+    [sym_headline] = STATE(3),
+    [aux_sym_org_file_repeat1] = STATE(3),
+    [ts_builtin_sym_end] = ACTIONS(7),
+    [sym_stars] = ACTIONS(5),
+  },
+  [3] = {
+    [sym__section] = STATE(3),
+    [sym_headline] = STATE(3),
+    [aux_sym_org_file_repeat1] = STATE(3),
+    [ts_builtin_sym_end] = ACTIONS(9),
+    [sym_stars] = ACTIONS(11),
   },
 };
 
 static uint16_t ts_small_parse_table[] = {
-  [0] = 1,
-    ACTIONS(5), 1,
+  [0] = 2,
+    ACTIONS(14), 1,
+      sym_text,
+    STATE(6), 1,
+      sym_title,
+  [7] = 1,
+    ACTIONS(16), 2,
       ts_builtin_sym_end,
-  [4] = 1,
-    ACTIONS(7), 1,
+      sym_stars,
+  [12] = 1,
+    ACTIONS(18), 2,
+      ts_builtin_sym_end,
+      sym_stars,
+  [17] = 1,
+    ACTIONS(20), 1,
+      sym_whitespace,
+  [21] = 1,
+    ACTIONS(22), 1,
       ts_builtin_sym_end,
 };
 
 static uint32_t ts_small_parse_table_map[] = {
-  [SMALL_STATE(2)] = 0,
-  [SMALL_STATE(3)] = 4,
+  [SMALL_STATE(4)] = 0,
+  [SMALL_STATE(5)] = 7,
+  [SMALL_STATE(6)] = 12,
+  [SMALL_STATE(7)] = 17,
+  [SMALL_STATE(8)] = 21,
 };
 
 static TSParseActionEntry ts_parse_actions[] = {
   [0] = {.entry = {.count = 0, .reusable = false}},
   [1] = {.entry = {.count = 1, .reusable = false}}, RECOVER(),
-  [3] = {.entry = {.count = 1, .reusable = true}}, SHIFT(2),
-  [5] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_source_file, 1),
-  [7] = {.entry = {.count = 1, .reusable = true}},  ACCEPT_INPUT(),
+  [3] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_org_file, 0),
+  [5] = {.entry = {.count = 1, .reusable = true}}, SHIFT(7),
+  [7] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_org_file, 1),
+  [9] = {.entry = {.count = 1, .reusable = true}}, REDUCE(aux_sym_org_file_repeat1, 2),
+  [11] = {.entry = {.count = 2, .reusable = true}}, REDUCE(aux_sym_org_file_repeat1, 2), SHIFT_REPEAT(7),
+  [14] = {.entry = {.count = 1, .reusable = true}}, SHIFT(5),
+  [16] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_title, 1),
+  [18] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_headline, 3),
+  [20] = {.entry = {.count = 1, .reusable = true}}, SHIFT(4),
+  [22] = {.entry = {.count = 1, .reusable = true}},  ACCEPT_INPUT(),
 };
 
 #ifdef __cplusplus
