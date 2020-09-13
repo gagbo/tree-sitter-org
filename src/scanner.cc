5567799f6f89c8eebe7180999a4cf1af6efbab09
Gerry Agbobada <gagbobada+git@gmail.com>
2020-09-13 17:55:32 +0200

    Compile with C++17 standard

--- src/scanner.cc
+++ src/scanner.cc
@@ -1,8 +1,25 @@
 #include <tree_sitter/parser.h>
+#include <memory>
 
 enum TokenType {
   ORG_HEADLINE,
 };
+
+
+class Scanner {
+    public:
+        /// Serialize the scanner to bytes
+        /// Return the count of bytes written
+        unsigned int serialize(char* buffer) const;
+        friend std::unique_ptr<Scanner> deserialize(const char* buffer, unsigned int length);
+        /// Scan the stream to find the correct token type
+        bool scan(TSLexer* lexer, const bool*valid_symbols);
+};
+
+/// Deserialize a buffer and modifies the current scanner to fit this
+std::unique_ptr<Scanner> deserialize(const char* buffer, unsigned int length) {
+    return std::make_unique<Scanner>();
+}
 
 extern "C" {
 void *tree_sitter_org_external_scanner_create();
@@ -15,6 +32,7 @@ void tree_sitter_org_external_scanner_deserialize(void *payload,
 bool tree_sitter_org_external_scanner_scan(void *payload, TSLexer *lexer,
                                            const bool *valid_symbols);
 }
+
 
 void *tree_sitter_org_external_scanner_create() { return nullptr; }
 
